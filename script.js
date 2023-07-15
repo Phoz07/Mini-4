e2202350f1a6724af3d4dcccb342343e232e7a74
Phoz07 <sakdithachpinkaew@gmail.com>
2023-07-15 23:24:25 +0700

    just commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const countersEl = document.querySelectorAll(".counter");
+
+countersEl.forEach(counter => {
+    counter.innerText = '0';
+    const updateCounter = () => {
+        const target = +counter.getAttribute("data-target");
+        const start = +counter.innerText;
+        const increment = target/200;
+        if (start < target){
+            counter.innerText = `${Math.ceil(start + increment)}`
+            setTimeout(updateCounter, 1)
+        }else {
+            counter.innerText.target
+        }
+    }
+    updateCounter();
+})
